fa3e681a50637446da496ba4cbbb858a636c2327
yzimhao <liaoshp@gmail.com>
2017-04-25 16:56:13 +0800

    webui

--- main.go
+++ main.go
@@ -1,53 +1,41 @@
 package main
 
 import (
-    // io "io/ioutil"
+    "io/ioutil"
     "encoding/json"
-    // "github.com/bitly/go-simplejson"
+    "github.com/bitly/go-simplejson"
     redis "github.com/go-redis/redis"
     "github.com/apsdehal/go-logger"
+    "github.com/gin-gonic/gin"
     "regexp"
     // "strconv"
     "os"
-
+    "flag"
     "fmt"
 )
 
+
+var log *logger.Logger
+var options *simplejson.Json
+
+
 type Info struct {
+    Redis_version string `json:"redis_version"`
+    Process_id string `json:"process_id"`
+    Uptime_in_days string `json:"uptime_in_days"`
 
     // # Clients
-    // connected_clients:1
+    Connected_clients string `json:"connected_clients"`
 
     // # Memory
     Used_memory string    `json:"userd_memory"`
     Used_memory_human string    `json:"userd_memory_human"`
-    // used_memory_rss:3600384
-    // used_memory_peak:815960
-    // used_memory_peak_human:796.84K
-    // used_memory_lua:36864
 
-    // # Persistence
-    // loading:0
-    // rdb_changes_since_last_save:20
-    // rdb_bgsave_in_progress:0
-    // rdb_last_save_time:1491979572
-    // rdb_last_bgsave_status:ok
-    // rdb_last_bgsave_time_sec:-1
-    // rdb_current_bgsave_time_sec:-1
-    //
-    //
-    // // # Stats
-    //
-    // expired_keys:1
-    // evicted_keys:0
     Keyspace_hits string    `json:"keyspace_hits"`
     Keyspace_misses string    `json:"keyspace_misses"`
-    //
-    //
-    // // # CPU
-    // used_cpu_sys:318.30
-    // used_cpu_user:117.02
 }
+
+
 
 
 
@@ -58,39 +46,83 @@ func reg_value(info string, regs string) string {
 }
 
 func get_info(info string) *Info {
-    khit := reg_value(info, `keyspace_hits:([\d]+)`)
-    kms := reg_value(info, `keyspace_misses:([\d]+)`)
-    um := reg_value(info, `used_memory:(\w+)`)
-    umh := reg_value(info, `used_memory_human:(\w\S+)`)
-
     data := &Info{
-        Used_memory: um,
-        Used_memory_human: umh,
-        Keyspace_hits: khit,
-        Keyspace_misses: kms}
+        Redis_version: reg_value(info, `redis_version:(\w\S+)`),
+        Process_id: reg_value(info, `process_id:(\w+)`),
+        Uptime_in_days: reg_value(info, `uptime_in_days:(\w+)`),
+        Connected_clients: reg_value(info, `connected_clients:(\w+)`),
+        Used_memory: reg_value(info, `used_memory:(\w+)`),
+        Used_memory_human: reg_value(info, `used_memory_human:(\w\S+)`),
+        Keyspace_hits: reg_value(info, `keyspace_hits:([\d]+)`),
+        Keyspace_misses: reg_value(info, `keyspace_misses:([\d]+)`)}
     return data
 }
 
 
-func main(){
-    log, err := logger.New("my", 1, os.Stdout)
-
+func monitor_redis(addr string, password string) *Info {
     client := redis.NewClient(&redis.Options{
-        Addr:     "127.0.0.1:6379",
-        Password: "", // no password set
+        Addr:     addr,
+        Password: password, // no password set
         DB:       0,  // use default DB
     })
-
 
     info, err := client.Info().Result()
     if err != nil {
         log.Error(err.Error())
         os.Exit(-1)
+    }else{
+        log.Info("redis client is ok!")
+    }
+    client.Close()
+    c := get_info(info)
+    return c
+}
+
+/**
+ * [解析配置文件]
+ * @type {[type]}
+ */
+
+func parse_config(file string){
+    bytes, e := ioutil.ReadFile(file)
+    if e != nil {
+        log.Error(e.Error())
+        os.Exit(1)
     }
 
+    opt, err := simplejson.NewJson(bytes)
+    if err != nil {
+        log.Error(err.Error())
+        os.Exit(1)
+    }
+    options = opt
+}
 
-    c := get_info(info)
-    b, _ := json.Marshal(c)
-    fmt.Println(c)
-    fmt.Println(string(b))
+
+func main(){
+    log, _ = logger.New("main", 1, os.Stdout)
+
+    // 解析cli 参数设置
+    conf := flag.String("c", "./redis-adm-config.sample", "config file path")
+    parse_config(*conf)
+    flag.Parse()
+
+
+    // fmt.Println(*conf)
+    // fmt.Println(options.Get("redis").Array())
+
+
+    // service
+    // gin.SetMode(gin.ReleaseMode)
+    r := gin.Default()
+
+    r.GET("/get/info", func(c *gin.Context){
+        cc := monitor_redis("127.0.0.1:6379", "")
+        b, _ := json.Marshal(cc)
+        callback := c.Query("callback")
+        res := fmt.Sprintf("%s(%s)", callback, string(b))
+        c.String(200, res)
+    })
+
+    r.Run(":5001")
 }
